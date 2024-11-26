97baefe4b91a83dd3f0354980dfef61f8aece4d8
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-11-26 15:33:23 +0200

    updated to use grafana url

--- src/components/grafana-panel.tsx
+++ src/components/grafana-panel.tsx
@@ -1,8 +1,10 @@
+import { env } from "~/env"
+
 export function GrafanaPanel() {
     return (
       <div className="bg-gray-800 rounded-lg shadow-xl p-6">
         <iframe
-          src="http://localhost:3000/d-solo/fe4lbp0qww6bke/apache-doris-dashboard?orgId=1&from=1732600984941&to=1732622584941&timezone=browser&theme=dark&panelId=2&__feature.dashboardSceneSolo"
+          src={`${env.GRAFANA_URL}/d-solo/fe4lbp0qww6bke/apache-doris-dashboard?orgId=1&from=1732600984941&to=1732622584941&timezone=browser&theme=dark&panelId=2&__feature.dashboardSceneSolo`}
           width="100%"
           height="400"
           frameBorder="0"
